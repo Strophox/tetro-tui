bdb911ea4c66751cd5a7d03d4deca4941fffadce
Strophox <strophox@gmail.com>
2026-03-25 03:52:29 +0100

    fix: Correct cheese gamemode implementation

--- src/game_modes/game_modifiers/cheese.rs
+++ src/game_modes/game_modifiers/cheese.rs
@@ -13,8 +13,9 @@ pub struct Cheese {
     tiles_per_line: NonZeroUsize,
     cheese_limit: Option<NonZeroU32>,
 
-    cheese_remaining: u32,
-    full_lines_pre_lineclear: usize,
+    cheese_eaten: u32,
+    cheese_last_eaten: usize,
+    cheese_generated: u32,
 }
 
 impl Cheese {
@@ -28,8 +29,9 @@ impl Cheese {
         let modifier = Box::new(Self {
             tiles_per_line,
             cheese_limit,
-            cheese_remaining: cheese_limit.unwrap_or(NonZeroU32::MAX).get(),
-            full_lines_pre_lineclear: 0,
+            cheese_eaten: 0,
+            cheese_last_eaten: 0,
+            cheese_generated: 0,
         });
 
         builder
@@ -56,28 +58,20 @@ impl GameModifier for Cheese {
     }
 
     fn on_game_built(&mut self, game: GameAccess) {
-        let n_init_lines = usize::try_from(self.cheese_remaining.min(10)).unwrap();
-
-        let mut cheese_lines = Self::prng_cheese_lines(
+        let cheese_lines = Self::prng_cheese_lines(
             self.tiles_per_line,
-            &mut self.cheese_remaining,
+            &self.cheese_limit,
+            &mut self.cheese_generated,
             &mut game.state.rng,
         );
 
-        for (line, cheese) in game
-            .state
-            .board
-            .iter_mut()
-            .take(n_init_lines)
-            .rev()
-            .zip(&mut cheese_lines)
-        {
+        for (line, cheese) in game.state.board.iter_mut().take(10).zip(cheese_lines) {
             *line = cheese;
         }
     }
 
     fn on_lock_post(&mut self, game: GameAccess, _feed: &mut NotificationFeed) {
-        self.full_lines_pre_lineclear = 0;
+        self.cheese_last_eaten = 0;
 
         // Check entire board.
         for line in game.state.board.iter() {
@@ -89,10 +83,9 @@ impl GameModifier for Cheese {
                     .any(|cell| *cell == Some(NonZeroU8::try_from(254).unwrap()))
                 {
                     // In theory would never underflow.
-                    self.cheese_remaining = self.cheese_remaining.saturating_sub(1);
+                    self.cheese_eaten += 1;
+                    self.cheese_last_eaten += 1;
                 }
-
-                self.full_lines_pre_lineclear += 1;
             }
         }
     }
@@ -100,37 +93,40 @@ impl GameModifier for Cheese {
     fn on_lines_clear_post(&mut self, game: GameAccess, _feed: &mut NotificationFeed) {
         let cheese_lines = Self::prng_cheese_lines(
             self.tiles_per_line,
-            &mut self.cheese_remaining,
+            &self.cheese_limit,
+            &mut self.cheese_generated,
             &mut game.state.rng,
         );
 
-        for cheese in cheese_lines.take(self.full_lines_pre_lineclear) {
+        for cheese in cheese_lines.take(self.cheese_last_eaten) {
             game.state.board.rotate_right(1);
             game.state.board[0] = cheese;
         }
+
+        game.state.lineclears = self.cheese_eaten;
     }
 }
 
 impl Cheese {
     fn prng_cheese_lines<'a>(
-        cheese_tiles_per_line: NonZeroUsize,
-        remaining: &'a mut u32,
+        tiles_per_line: NonZeroUsize,
+        limit: &'a Option<NonZeroU32>,
+        generated: &'a mut u32,
         rng: &'a mut GameRng,
     ) -> impl Iterator<Item = Line> + 'a {
         let grey_tile = Some(NonZeroU8::try_from(254).unwrap());
         std::iter::from_fn(move || {
-            if *remaining > 0 {
-                *remaining -= 1;
+            limit.is_none_or(|l| *generated < l.get()).then(|| {
+                *generated += 1;
                 let mut line = Line::default();
-                for tile in line.iter_mut().take(cheese_tiles_per_line.get()) {
+                for tile in line.iter_mut().take(tiles_per_line.get()) {
                     *tile = grey_tile;
                 }
                 // Currently completely random.
                 line.shuffle(rng);
-                Some(line)
-            } else {
-                None
-            }
+
+                line
+            })
         })
     }
 }
