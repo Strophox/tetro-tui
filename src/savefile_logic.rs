2faca850aa1a9a8579527c3c0ff90a3eabc97633
Strophox <strophox@gmail.com>
2026-04-06 20:57:15 +0200

    refactor: Ensure savefile de/serialization always match

--- src/savefile_logic.rs
+++ src/savefile_logic.rs
@@ -1,11 +1,13 @@
 use std::{
+    borrow::Cow,
     fs::File,
     io::{self, Read, Write},
     path::PathBuf,
 };
 
 use crate::{
-    game_restoration::RawInputHistory, Application, CompressedInputHistory, GameSave, GameSaves,
+    game_restoration::RawInputHistory, settings::Settings, Application, CompressedInputHistory,
+    GameSave, GameSaves, Scoreboard, Statistics,
 };
 
 pub fn savefile_name() -> String {
@@ -39,11 +41,22 @@ pub enum SavefileGranularity {
     RememberSettingsScoresReplays,
 }
 
+#[derive(serde::Serialize, serde::Deserialize)]
+struct SavefileContents<'a> {
+    save_on_exit: SavefileGranularity,
+    statistics: Cow<'a, Statistics>,
+    game_saves: GameSaves<CompressedInputHistory>,
+    settings: Cow<'a, Settings>,
+    scores_and_replays: Cow<'a, Scoreboard>,
+}
+
 impl<T: Write> Application<T> {
     pub fn load_from_savefile(&mut self) -> io::Result<()> {
         let mut file = File::open(self.temp_data.savefile_path.clone())?;
         let mut save_str = String::new();
         file.read_to_string(&mut save_str)?;
+
+        let save_loaded: SavefileContents = serde_json::from_str(&save_str)?;
 
         // Make sure no field is forgotten by explicitly unpacking.
         let Application {
@@ -54,26 +67,21 @@ impl<T: Write> Application<T> {
             statistics,
             game_saves,
         } = self;
-        let compressed_game_saves: GameSaves<CompressedInputHistory>;
 
-        (
-            temp_data.save_on_exit,
-            *settings,
-            *scores_and_replays,
-            *statistics,
-            compressed_game_saves,
-        ) = serde_json::from_str(&save_str)?;
-
-        let raw_game_saves = compressed_game_saves
+        temp_data.save_on_exit = save_loaded.save_on_exit;
+        *settings = save_loaded.settings.into_owned();
+        *scores_and_replays = save_loaded.scores_and_replays.into_owned();
+        *statistics = save_loaded.statistics.into_owned();
+        game_saves.slots = save_loaded
+            .game_saves
             .slots
             .into_iter()
             .filter_map(|save| save.decompress())
             .collect::<Vec<GameSave<RawInputHistory>>>();
-
-        game_saves.pick = compressed_game_saves
+        game_saves.pick = save_loaded
+            .game_saves
             .pick
-            .min(raw_game_saves.len().saturating_sub(1));
-        game_saves.slots = raw_game_saves;
+            .min(game_saves.slots.len().saturating_sub(1));
 
         Ok(())
     }
@@ -100,13 +108,15 @@ impl<T: Write> Application<T> {
                 .collect::<Vec<_>>(),
         };
 
-        let save_str = serde_json::to_string(&(
-            self.temp_data.save_on_exit,
-            &self.settings,
-            &self.scores_and_replays,
-            &self.statistics,
-            compressed_game_saves,
-        ))?;
+        let savefile_contents = SavefileContents {
+            save_on_exit: self.temp_data.save_on_exit,
+            settings: Cow::Borrowed(&self.settings),
+            scores_and_replays: Cow::Borrowed(&self.scores_and_replays),
+            statistics: Cow::Borrowed(&self.statistics),
+            game_saves: compressed_game_saves,
+        };
+
+        let save_str = serde_json::to_string(&savefile_contents)?;
 
         let mut file = File::create(self.temp_data.savefile_path.clone())?;
         let n_written = file.write(save_str.as_bytes())?;
