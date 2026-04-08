b752ef0e581d43ecfeb1e9781844df6c5ce3eac1
Strophox <strophox@gmail.com>
2026-04-08 15:07:50 +0200

    refactor: formatting

--- src/game_renderers/standard_buffered.rs
+++ src/game_renderers/standard_buffered.rs
@@ -43,6 +43,115 @@ pub trait TerminalBuffer {
     serde::Serialize,
     serde::Deserialize,
 )]
+struct DenseTerminalDoubleBuffer {
+    width: u16,
+    height: u16,
+    /// INVARIANT:
+    /// - `prev_buf.len() == width * height`.
+    prev_buf: Vec<TermCell>,
+    /// INVARIANT:
+    /// - `next_buf.len() == width * height`.
+    next_buf: Vec<TermCell>,
+}
+
+impl TerminalBuffer for DenseTerminalDoubleBuffer {
+    fn with_dimensions(width: u16, height: u16) -> Self {
+        DenseTerminalDoubleBuffer {
+            width,
+            height,
+            prev_buf: vec![EMPTY_CELL; (width * height).into()],
+            next_buf: vec![EMPTY_CELL; (width * height).into()],
+        }
+    }
+
+    fn dimensions(&self) -> (u16, u16) {
+        (self.width, self.height)
+    }
+
+    fn reset_with_dimensions(&mut self, width: u16, height: u16) {
+        let old_len = (self.width * self.height).into();
+        let new_len = (width * height).into();
+        if new_len > old_len {
+            self.prev_buf.fill(EMPTY_CELL);
+            self.next_buf.fill(EMPTY_CELL);
+            self.prev_buf.resize(new_len, EMPTY_CELL);
+            self.next_buf.resize(new_len, EMPTY_CELL);
+        } else {
+            self.prev_buf.fill(EMPTY_CELL);
+            self.next_buf.fill(EMPTY_CELL);
+            self.prev_buf.resize(new_len, EMPTY_CELL);
+            self.next_buf.resize(new_len, EMPTY_CELL);
+        }
+        self.width = width;
+        self.height = height;
+    }
+
+    fn write(&mut self, x: u16, y: u16, cell: TermCell) {
+        if x < self.width && y < self.height {
+            let idx = x as usize + self.width as usize * y as usize;
+            self.next_buf[idx] = cell;
+        }
+    }
+
+    fn write_str(&mut self, x: u16, y: u16, str: &str, fg: Color) {
+        if y >= self.height {
+            return;
+        }
+        for (dx, ch) in str.chars().enumerate() {
+            if x + dx as u16 >= self.width {
+                return;
+            }
+            let idx = x as usize + dx + self.width as usize * y as usize;
+            self.next_buf[idx] = TermCell { ch, fg };
+        }
+    }
+
+    fn flush(&mut self, term: &mut impl Write) -> io::Result<()> {
+        term.queue(terminal::BeginSynchronizedUpdate)?;
+
+        for x in 0..self.width {
+            for y in 0..self.height {
+                let idx = x as usize + self.width as usize * y as usize;
+                #[rustfmt::skip] let TermCell { ch: old_ch, fg: old_fg } = self.prev_buf[idx];
+                #[rustfmt::skip] let TermCell { ch: new_ch, fg: new_fg } = self.next_buf[idx];
+
+                term.queue(cursor::MoveTo(x, y))?;
+                if new_fg != old_fg {
+                    // Always reprint styled if style changed.
+                    term.queue(PrintStyledContent(new_ch.with(new_fg)))?;
+                } else if new_ch != old_ch {
+                    // Style did not change, but character did, so reprint it.
+                    term.queue(Print(new_ch))?;
+                }
+            }
+        }
+
+        term.queue(cursor::MoveTo(0, 0))?
+            .queue(terminal::EndSynchronizedUpdate)?
+            .flush()?;
+
+        // Swap buffers so `prev_buf` correctly contains the one we just wrote and want to keep for next time.
+        std::mem::swap(&mut self.prev_buf, &mut self.next_buf);
+
+        // Reset buffer by overwriting nonempty cells.
+        self.next_buf.fill(EMPTY_CELL);
+
+        Ok(())
+    }
+}
+
+#[derive(
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    Clone,
+    Debug,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+)]
 struct SparseTerminalBuffer {
     width: u16,
     height: u16,
@@ -116,16 +225,10 @@ impl TerminalBuffer for SparseTerminalBuffer {
                 // Both are empty, nothing to do.
                 (None, None) => break,
 
+                #[rustfmt::skip]
                 // Old buffer contains something the new one doesn't: Overwrite it to clear it.
-                (
-                    Some((
-                        old_pos,
-                        TermCell {
-                            ch: _old_ch,
-                            fg: old_fg,
-                        },
-                    )),
-                    None,
+                (Some((old_pos, TermCell { ch: _old_ch, fg: old_fg })),
+                 None
                 ) => {
                     // Only explicitly reset color if necessary.
                     let new_fg = (*old_fg != Color::Reset).then_some(Color::Reset);
@@ -134,16 +237,10 @@ impl TerminalBuffer for SparseTerminalBuffer {
                     old_pos_cell = old_buffer.next();
                 }
 
+                #[rustfmt::skip]
                 // New buffer contains something the old one doesn't: Write it.
-                (
-                    None,
-                    Some((
-                        new_x_y,
-                        TermCell {
-                            ch: new_ch,
-                            fg: new_fg,
-                        },
-                    )),
+                (None,
+                 Some((new_x_y, TermCell { ch: new_ch, fg: new_fg })),
                 ) => {
                     // Only explicitly reset color if necessary.
                     let new_fg = (*new_fg != Color::Reset).then_some(*new_fg);
@@ -152,21 +249,9 @@ impl TerminalBuffer for SparseTerminalBuffer {
                     new_pos_cell = new_buffer.next();
                 }
 
-                (
-                    Some((
-                        old_pos,
-                        TermCell {
-                            ch: old_ch,
-                            fg: old_fg,
-                        },
-                    )),
-                    Some((
-                        new_pos,
-                        TermCell {
-                            ch: new_ch,
-                            fg: new_fg,
-                        },
-                    )),
+                #[rustfmt::skip]
+                (Some((old_pos, TermCell { ch: old_ch, fg: old_fg })),
+                 Some((new_pos, TermCell { ch: new_ch, fg: new_fg })),
                 ) => {
                     match old_pos.cmp(new_pos) {
                         // Old buffer contains something the new one doesn't: Overwrite it to clear it.
@@ -208,121 +293,6 @@ impl TerminalBuffer for SparseTerminalBuffer {
         term.queue(cursor::MoveTo(0, 0))?
             .queue(terminal::EndSynchronizedUpdate)?
             .flush()?;
-
-        Ok(())
-    }
-}
-
-#[derive(
-    PartialEq,
-    Eq,
-    PartialOrd,
-    Ord,
-    Hash,
-    Clone,
-    Debug,
-    Default,
-    serde::Serialize,
-    serde::Deserialize,
-)]
-struct DenseTerminalDoubleBuffer {
-    width: u16,
-    height: u16,
-    /// INVARIANT:
-    /// - `prev_buf.len() == width * height`.
-    prev_buf: Vec<TermCell>,
-    /// INVARIANT:
-    /// - `next_buf.len() == width * height`.
-    next_buf: Vec<TermCell>,
-}
-
-impl TerminalBuffer for DenseTerminalDoubleBuffer {
-    fn with_dimensions(width: u16, height: u16) -> Self {
-        DenseTerminalDoubleBuffer {
-            width,
-            height,
-            prev_buf: vec![EMPTY_CELL; (width * height).into()],
-            next_buf: vec![EMPTY_CELL; (width * height).into()],
-        }
-    }
-
-    fn dimensions(&self) -> (u16, u16) {
-        (self.width, self.height)
-    }
-
-    fn reset_with_dimensions(&mut self, width: u16, height: u16) {
-        let old_len = (self.width * self.height).into();
-        let new_len = (width * height).into();
-        if new_len > old_len {
-            self.prev_buf.fill(EMPTY_CELL);
-            self.next_buf.fill(EMPTY_CELL);
-            self.prev_buf.resize(new_len, EMPTY_CELL);
-            self.next_buf.resize(new_len, EMPTY_CELL);
-        } else {
-            self.prev_buf.fill(EMPTY_CELL);
-            self.next_buf.fill(EMPTY_CELL);
-            self.prev_buf.resize(new_len, EMPTY_CELL);
-            self.next_buf.resize(new_len, EMPTY_CELL);
-        }
-        self.width = width;
-        self.height = height;
-    }
-
-    fn write(&mut self, x: u16, y: u16, cell: TermCell) {
-        if x < self.width && y < self.height {
-            let idx = x as usize + self.width as usize * y as usize;
-            self.next_buf[idx] = cell;
-        }
-    }
-
-    fn write_str(&mut self, x: u16, y: u16, str: &str, fg: Color) {
-        if y >= self.height {
-            return;
-        }
-        for (dx, ch) in str.chars().enumerate() {
-            if x + dx as u16 >= self.width {
-                return;
-            }
-            let idx = x as usize + dx + self.width as usize * y as usize;
-            self.next_buf[idx] = TermCell { ch, fg };
-        }
-    }
-
-    fn flush(&mut self, term: &mut impl Write) -> io::Result<()> {
-        term.queue(terminal::BeginSynchronizedUpdate)?;
-
-        for x in 0..self.width {
-            for y in 0..self.height {
-                let idx = x as usize + self.width as usize * y as usize;
-                let TermCell {
-                    ch: old_ch,
-                    fg: old_fg,
-                } = self.prev_buf[idx];
-                let TermCell {
-                    ch: new_ch,
-                    fg: new_fg,
-                } = self.next_buf[idx];
-
-                term.queue(cursor::MoveTo(x, y))?;
-                if new_fg != old_fg {
-                    // Always reprint styled if style changed.
-                    term.queue(PrintStyledContent(new_ch.with(new_fg)))?;
-                } else if new_ch != old_ch {
-                    // Style did not change, but character did, so reprint it.
-                    term.queue(Print(new_ch))?;
-                }
-            }
-        }
-
-        term.queue(cursor::MoveTo(0, 0))?
-            .queue(terminal::EndSynchronizedUpdate)?
-            .flush()?;
-
-        // Swap buffers so `prev_buf` correctly contains the one we just wrote and want to keep for next time.
-        std::mem::swap(&mut self.prev_buf, &mut self.next_buf);
-
-        // Reset buffer by overwriting nonempty cells.
-        self.next_buf.fill(EMPTY_CELL);
 
         Ok(())
     }
