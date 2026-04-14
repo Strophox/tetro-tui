e06e614e58e636c6c8c4638bcb9488336fad7d12
Strophox <strophox@gmail.com>
2026-04-15 01:46:36 +0200

    implement LineClearParticleEffect (yippie)

--- src/tui_settings/mod.rs
+++ src/tui_settings/mod.rs
@@ -15,7 +15,7 @@ pub use graphics_settings::{
     palette::Palette,
     small_tet_style::SmallTetStyle,
     tui_style::TuiStyle,
-    GraphicsSettings, TileTexture,
+    GraphicsSettings, QuickTileFromStr, TileTexture,
 };
 
 use crate::{
